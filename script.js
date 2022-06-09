c61880ed4a9e055b39ab643c5c929c24a8ebc348
jsaique <106097322+jsaique@users.noreply.github.com>
2022-06-09 14:45:24 -0400

    Fixing the body color when answer is correct

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const displayNumber = function (number) {
   document.querySelector('.number').textContent = number;
 };
 const displayBody = function (body) {
-  document.querySelector(body).style.backgroundColor = body;
+  document.querySelector('body').style.backgroundColor = body;
 };
 const displayScore = function (score1) {
   document.querySelector('.score').textContent = score1;
